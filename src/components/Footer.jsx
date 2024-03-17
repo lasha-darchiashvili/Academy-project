e7e2d19edc165b5b6cb8db0a9a47358d32210a97
lasha-darchiashvili <ldarc17@freeuni.edu.ge>
2024-03-18 02:57:37 +0400

    feat: add cards

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -64,7 +64,7 @@ export default function Footer() {
                 />
                 <button
                   type="submit"
-                  className="py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+                  className="py-2 px-4 bg-gray-500 text-white rounded-md hover:bg-gray-700 focus:outline-none"
                 >
                   Subscribe
                 </button>
