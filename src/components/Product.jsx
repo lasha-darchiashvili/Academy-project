3ca88909ec730ff81e616b92feaf61503d950eb8
lasha-darchiashvili <ldarc17@freeuni.edu.ge>
2024-03-18 03:23:50 +0400

    fix: cards props minor fix

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,19 +1,14 @@
 import React from "react";
-import ac from "../assets/ac.webp";
 
-export default function Product() {
+export default function Product({ name, description, image }) {
   return (
     <div class="bg-white rounded-lg p-4">
       <div className="w-[20rem] mx-auto ">
-        <img className="rounded-lg" src={ac} alt="productphoto" />
+        <img className="rounded-lg" src={image} alt="productphoto" />
       </div>
       <div className="w-[20rem] mx-auto mt-[1rem]">
-        <div class="text-[1.4rem] font-semibold">Assassin's Creed</div>
-        <div class="text-gray-700 mt-[0.5rem]">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium
-          ipsa numquam sed voluptas dolore eaque provident error, quo nam
-          incidunt esse expedita consequatur necessitatibus.
-        </div>
+        <div class="text-[1.4rem] font-semibold">{name}</div>
+        <div class="text-gray-700 mt-[0.5rem]">{description}</div>
         <button class="w-full mt-[0.5rem] bg-gray-500 text-white px-4 py-2 rounded-md hover:bg-gray-700 focus:outline-none">
           Add to Cart
         </button>
