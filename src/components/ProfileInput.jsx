913e1405be2a6c277b35c579d4f9c504ac7ba02a
lasha-darchiashvili <ldarc17@freeuni.edu.ge>
2024-03-31 16:56:40 +0400

    feat: add state to profile page inputs

--- src/components/ProfileInput.jsx
+++ src/components/ProfileInput.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 
-export default function ProfileInput({ inputValue }) {
+export default function ProfileInput({
+  onChange,
+  value,
+  name,
+  type,
+  placeholder,
+  title,
+}) {
   return (
-    <input
-      type="text"
-      value={inputValue}
-      className="mt-[1rem] text-[1.2em] rounded py-4 px-3 text-black leading-tight focus:outline-none focus:shadow-outline bg-white"
-    />
+    <div className="flex flex-col gap-[1rem]">
+      <label className="block text-gray-400 text-[1.6rem] font-semibold">
+        {title}
+      </label>
+      <input
+        onChange={onChange}
+        value={value}
+        name={name}
+        type={type}
+        placeholder={placeholder}
+        className="w-[30rem] text-[1.2em] rounded py-4 px-3 text-black leading-tight focus:outline-none focus:shadow-outline bg-white"
+      />
+      <hr className="border-gray-400" />
+    </div>
   );
 }
