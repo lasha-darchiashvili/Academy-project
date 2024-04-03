22526af349b98c732e3d1d1b883ab7233e06e089
lasha-darchiashvili <ldarc17@freeuni.edu.ge>
2024-04-04 01:56:36 +0400

    refactor: installing next js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
-      colors: {
-        "custom-black": "#000000",
-        "custom-white": "#FFFFFF",
-        "custom-gray": "#8B888C",
-        "custom-blue": "#1A161F",
-        "custom-brown": "#2E2938",
-        "custom-second-blue": "#1C2B39",
-      },
-      fontFamily: {
-        roboto: ["Roboto", "sans-serif"],
-        poppins: ["Poppins", "sans-serif"],
-        nunito: ["Nunito", "sans-serif"],
-        raleway: ["Raleway", "sans-serif"],
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
